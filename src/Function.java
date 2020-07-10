abcc59a8a8e015a3cfe14141defee206e6da2c5c
Aris Lemmenes <aris.lemmenes@gmail.com>
2020-07-10 17:42:21 -0500

    added coordinate

---
+++ src/Function.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Function{
+
+    String functionString;
+
+    public Function(String functionString){
+        this.functionString = functionString;
+    }
+    public createFunctionTree(int nodeNumber){
+        for(int i = 0; i < nodeNumber; i ++){
+            
+        }
+    }
+    
+
+}
